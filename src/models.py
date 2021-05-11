fix the History class for gpu computation

src/models.py
@@ -375,14 +375,14 @@ class Tree_List:
                     device=self.device) for i in composition_list]
             batch_composition_info[idx] = torch.stack(
                 [torch.cat((i, j)) for (i, j) in zip(composition_list, dummy_compositin_info)])
+
         # return zipped batch information, when training extract each batch from zip itteration
         return zip(
             batch_leaf_content_id,
             content_mask,
             batch_composition_info,
             batch_label_list,
-            label_mask
-        )
+            label_mask)
 
     def replace_vocab_category(self, tree_list):
         self.content_to_id = tree_list.content_to_id
@@ -461,39 +461,54 @@ class History:
         self.tree_list = tree_list
         self.criteria = criteria
         self.THRESHOLD = THRESHOLD
+        self.batch_loss = []
+        self.batch_acc = []
         self.loss_history = np.array([])
         self.acc_history = np.array([])
 
-    def cal_stat(self):
-        loss = 0.0
-        acc = 0.0
-        for tree in self.tree_list.tree_list:
-            leaf_node_info = tree.leaf_node_info
-            label_list = tree.label_list
-            composition_info = tree.composition_info
-            output = self.tree_net(leaf_node_info, composition_info)
-            loss += self.criteria(output, label_list) / output.shape[0]
-            acc += self.cal_acc(output, label_list, self.THRESHOLD)
-        loss = loss.detach().cpu().numpy() / len(self.tree_list.tree_list)
-        acc = np.array(acc) / len(self.tree_list.tree_list)
+    # record the loss and acc of each batch and add them to list
+    def record_batch_loss_acc(self, loss, batch_output, batch_label, batch_label_mask):
+        self.batch_loss.append(loss.item())
+        self.batch_acc.append(self.cal_acc(batch_output, batch_label, batch_label_mask))
+
+    def record_loss_acc_for_all_batches(self):
+        loss = sum(self.batch_loss) / len(self.batch_loss)
+        acc = sum(self.batch_acc) / len(self.batch_acc)
         self.loss_history = np.append(self.loss_history, loss)
         self.acc_history = np.append(self.acc_history, acc)
         self.min_loss = np.min(self.loss_history)
         self.min_loss_idx = np.argmin(self.loss_history)
         self.max_acc = np.max(self.acc_history)
         self.max_acc_idx = np.argmax(self.acc_history)
+        # reset batch_loss and batch_acc for next epoch
+        self.batch_loss = []
+        self.batch_acc = []
 
-    def cal_acc(self, output, label_list, THRESHOLD):
-        num_correct = 0
-        for i in range(output.shape[0]):
-            for j in range(output.shape[1]):
-                if output[i][j] >= THRESHOLD:
-                    output[i][j] = 1.0
-                else:
-                    output[i][j] = 0.0
-            if torch.count_nonzero(output[i] == label_list[i]) == output.shape[1]:
-                num_correct += 1
-        return num_correct / output.shape[0]
+    # when calcurate stat for test data initial state and after each epoch
+    def cal_stat_from_batch_list(self, batch_list):
+        for batch in batch_list:
+            output = self.tree_net(batch)
+            label_list = batch[3]
+            label_mask = batch[4]
+            loss = self.criteria(output * label_mask, label_list)
+            self.record_batch_loss_acc(loss, output, label_list, label_mask)
+        self.record_loss_acc_for_all_batches()
+
+    def cal_acc(self, batch_output, batch_label, batch_label_mask):
+        # extract the element over threshold
+        prediction = batch_output > self.THRESHOLD
+        # matching the prediction and label, dummy node always become correct
+        matching = (prediction * batch_label_mask) == batch_label
+        matching = torch.count_nonzero(matching, dim=2) == batch_output.shape[2]
+        # the number of nodes the prediction is correct
+        num_correct_node = torch.count_nonzero(matching).item()
+        # the number of actually existing nodes(not a dummy nodes for fulling batch)
+        num_existing_node = torch.count_nonzero(torch.all(batch_label_mask, dim=2)).item()
+        # the number of dummy nodes
+        num_dummy_node = batch_label_mask.shape[0] * batch_label_mask.shape[1] - num_existing_node
+        # for the dummy nodes the prediction always become correct because they are zero
+        # so, subtruct them when calculate the acc
+        return (num_correct_node - num_dummy_node) / num_existing_node
 
     def print_current_stat(self, name):
         print('{}-loss: {}'.format(name, self.loss_history[-1]))
@@ -522,12 +537,13 @@ class Condition_Setter:
         else:
             self.RANDOM = True
             self.param_list.append('random')
-        if int(input("reg(0) or not_reg(1): ")) == 1:
-            self.REGULARIZED = False
-            self.param_list.append('not_reg')
-        else:
-            self.REGULARIZED = True
-            self.param_list.append('reg')
+        self.REGULARIZED = True
+        # if int(input("reg(0) or not_reg(1): ")) == 1:
+        #     self.REGULARIZED = False
+        #     self.param_list.append('not_reg')
+        # else:
+        #     self.REGULARIZED = True
+        #     self.param_list.append('reg')
         embedding_dim = input("embedding_dim(default=100d): ")
         if embedding_dim != "":
             self.embedding_dim = int(embedding_dim)
@@ -559,10 +575,10 @@ class Condition_Setter:
                 path_list[i] += "random"
             else:
                 path_list[i] += "GloVe"
-            if self.REGULARIZED:
-                path_list[i] += "_reg"
-            else:
-                path_list[i] += "_not_reg"
+            # if self.REGULARIZED:
+            #     path_list[i] += "_reg"
+            # else:
+            #     path_list[i] += "_not_reg"
             path_list[i] += "_" + str(self.embedding_dim) + "d"
         self.path_to_initial_weight_matrix = path_list[0] + \
             "_initial_weight_matrix.csv"

src/test1.py
@@ -4,18 +4,14 @@ import csv
 import random
 from torch.fft import fft, ifft
 from torch import conj, mul
+import time
 
+start = time.time()
+m = []
+for i in range(10000000):
+    m.append(i)
+print(time.time() - start)
 
-def circular_correlation(a, b):
-    a = conj(fft(a))
-    b = fft(b)
-    c = mul(a, b)
-    c = ifft(c).real
-    return c.div(c.norm(dim=1, keepdim=True) + 1e-6)
-
-
-a = torch.tensor([[1, 2, 3, 4, 5], [5, 4, 3, 2, 1], [2, 3, 4, 5, 6]])
-b = torch.tensor([[5, 4, 3, 2, 1], [1, 2, 3, 4, 5], [6, 5, 4, 3, 2]])
-
-c = circular_correlation(a, b)
-print(c)
+start = time.time()
+m = [i for i in range(10000000)]
+print(time.time() - start)