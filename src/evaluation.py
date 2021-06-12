f640a1661961a36382bb2636f6864a352f99486b
Ryosuke Yamaki <Ryosuke-Yamaki>
2021-06-12 18:40:46 +0900

    implemented projection_visualize.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -3,6 +3,7 @@ import os
 import torch
 from models import Tree_Net
 from utils import set_random_seed, Condition_Setter
+from utils import load
 
 PATH_TO_DIR = os.getcwd().replace("Hol-CCG/src", "")
 condition = Condition_Setter(PATH_TO_DIR)
@@ -12,13 +13,13 @@ device = torch.device('cpu')
 set_random_seed(0)
 
 print('loading tree list...')
-train_tree_list = load(PATH_TO_DIR + "Hol-CCG/data/train_tree_list.pickle")
-dev_tree_list = load(PATH_TO_DIR + "Hol-CCG/data/dev_tree_list.pickle")
 test_tree_list = load(PATH_TO_DIR + "Hol-CCG/data/test_tree_list.pickle")
 
-NUM_VOCAB = len(train_tree_list.content_vocab)
-NUM_CATEGORY = len(train_tree_list.category_vocab)
-tree_net = Tree_Net(NUM_VOCAB, NUM_CATEGORY, condition.embedding_dim).to(device)
+NUM_VOCAB = len(test_tree_list.content_vocab)
+NUM_CATEGORY = len(test_tree_list.category_vocab)
+
+tree_net = Tree_Net(NUM_VOCAB, NUM_CATEGORY,
+                    condition.embedding_dim).to(device)
 tree_net = torch.load(condition.path_to_model,
                       map_location=device)
 tree_net.eval()
