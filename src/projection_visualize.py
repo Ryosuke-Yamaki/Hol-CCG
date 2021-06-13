e654ad51ace32f033cbe88cefe400cb65c53c0ef
Ryosuke Yamaki <Ryosuke-Yamaki>
2021-06-13 19:07:24 +0900

    fix projection_vis

--- src/projection_visualize.py
+++ src/projection_visualize.py
@@ -2,15 +2,14 @@ from utils import load, load_weight_matrix
 from sklearn.decomposition import PCA
 from collections import Counter
 import os
-import torch
 import matplotlib.pyplot as plt
 from utils import set_random_seed, Condition_Setter
-from models import Tree_List, Tree_Net
-from sklearn.manifold import TSNE
-import numpy as np
+
 
 PATH_TO_DIR = os.getcwd().replace("Hol-CCG/src", "")
 condition = Condition_Setter(PATH_TO_DIR)
+
+set_random_seed(0)
 
 print("loading_weight_matrix...")
 initial_weight_matrix = load_weight_matrix(
