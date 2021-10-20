a043cbf1e90d9deceff62bcb4c6dd70eeb2c708e
Ryosuke Yamaki <Ryosuke-Yamaki>
2021-10-20 19:13:06 +0900

    modified train.py and evaluate_batch_list to record span stats

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+from torch.nn.functional import softmax, sigmoid
 import torch.nn as nn
 from tqdm import tqdm
 import os
@@ -129,22 +130,34 @@ def evaluate_tree_list(tree_list, tree_net, k_list=[1, 5]):
 
 
 @torch.no_grad()
-def evaluate_batch_list(batch_list, tree_net, criteria=nn.CrossEntropyLoss()):
+def evaluate_batch_list(
+        batch_list,
+        tree_net,
+        cat_criteria=nn.CrossEntropyLoss(),
+        span_criteria=nn.BCELoss()):
     print("evaluating...")
     num_word = 0
     num_phrase = 0
+    num_span = 0
     num_correct_word = 0
     num_correct_phrase = 0
+    num_correct_span = 0
     word_loss = 0
     phrase_loss = 0
+    span_loss = 0
     with tqdm(total=len(batch_list), unit="batch") as pbar:
         for batch in batch_list:
-            word_output, phrase_output, word_label, phrase_label = tree_net(batch)
+            word_output, phrase_output, span_output, word_label, phrase_label, span_label = tree_net(
+                batch)
+            span_output = sigmoid(span_output)
+
             num_word += word_output.shape[0]
             num_phrase += phrase_output.shape[0]
+            num_span += span_output.shape[0]
 
-            word_loss += criteria(word_output, word_label)
-            phrase_loss += criteria(phrase_output, phrase_label)
+            word_loss += cat_criteria(word_output, word_label)
+            phrase_loss += cat_criteria(phrase_output, phrase_label)
+            span_loss += span_criteria(span_output, span_label)
 
             # remove unknown categories
             word_output = word_output[word_label != 0]
@@ -155,22 +168,30 @@ def evaluate_batch_list(batch_list, tree_net, criteria=nn.CrossEntropyLoss()):
             num_correct_word += torch.count_nonzero(torch.argmax(word_output, dim=1) == word_label)
             num_correct_phrase += torch.count_nonzero(
                 torch.argmax(phrase_output, dim=1) == phrase_label)
+            span_output[span_output >= 0.5] = 1.0
+            span_output[span_output < 0.5] = 0.0
+            num_correct_span += torch.count_nonzero(span_output == span_label)
+
             pbar.update(1)
+
     word_loss = word_loss / len(batch_list)
     phrase_loss = phrase_loss / len(batch_list)
-    total_loss = word_loss + phrase_loss
-    total_acc = (num_correct_word + num_correct_phrase) / (num_word + num_phrase)
+    span_loss = span_loss / len(batch_list)
     word_acc = num_correct_word / num_word
     phrase_acc = num_correct_phrase / num_phrase
+    span_acc = num_correct_span / num_span
+
     stat = {
-        "total_acc": total_acc,
         "word_acc": word_acc,
         "phrase_acc": phrase_acc,
-        "total_loss": total_loss,
+        "span_acc": span_acc,
         "word_loss": word_loss,
-        "phrase_loss": phrase_loss}
-    print("total_acc:{}\nword_acc:{}\nphrase_acc:{}".format(
-        stat["total_acc"], stat["word_acc"], stat["phrase_acc"]))
+        "phrase_loss": phrase_loss,
+        "span_loss": span_loss}
+
+    print("word_acc:{}\nphrase_acc:{}\nspan_acc:{}".format(
+        stat["word_acc"], stat["phrase_acc"], stat["span_acc"]))
+
     return stat
 
 
