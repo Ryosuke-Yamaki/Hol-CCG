94ad1100e6a6e21d0af78dc915d6539cd1d77a37
Ryosuke Yamaki <Ryosuke-Yamaki>
2021-10-21 17:19:50 +0900

    implemented random sampling and recording of random seed

---
+++ src/repeat_train.sh
@@ -0,0 +1,13 @@
+#!~/Hol-CCG/src
+
+python train.py roberta-large True False
+python train.py roberta-large True False
+python train.py roberta-large True False
+python train.py roberta-large True False
+python train.py roberta-large True False
+
+python train.py roberta-large False False
+python train.py roberta-large False False
+python train.py roberta-large False False
+python train.py roberta-large False False
+python train.py roberta-large False False
