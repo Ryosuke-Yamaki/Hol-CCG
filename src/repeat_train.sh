2f2bbb70d9c16a6318529356bbbea30395dce40d
Ryosuke Yamaki <Ryosuke-Yamaki>
2021-10-22 23:26:40 +0900

    modified the setting of TARGET

--- src/repeat_train.sh
+++ src/repeat_train.sh
@@ -1,11 +1,5 @@
 #!~/Hol-CCG/src
 
-python train.py roberta-large True False
-python train.py roberta-large True False
-python train.py roberta-large True False
-python train.py roberta-large True False
-python train.py roberta-large True False
-
 python train.py roberta-large False False
 python train.py roberta-large False False
 python train.py roberta-large False False
